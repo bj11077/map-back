d51e621ee91b9a48b8b87e8cfcda2c13affe2162
bjune-park <54969248+bj11077@users.noreply.github.com>
2022-05-07 13:21:57 +0900

    init

---
+++ src/main/java/map/mapback/MapBackApplication.java
@@ -0,0 +1,13 @@
+package map.mapback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/map/mapback/MapBackApplicationTests.java
@@ -0,0 +1,13 @@
+package map.mapback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
